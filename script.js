81ff8ab60818263966b97f7b313943c5506f11d3
Kaio Prates <kaio_prates@hotmail.com>
2022-04-20 01:22:13 -0300

    Implement UI
    - Add content and style for the webpage;
    - Build interactive user input;
    - Add display player selection function;
    - Add display computer selection function;
    - Change results display to show on screen;
    - Change score to show on screen;

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
 // User inputs a choice, either: “rock”, “paper” or “scissor”
-function getUserPlay() {
-  const input = prompt('Rock, Paper or Scissor?');
-  const inputEdited = input.toString().toLowerCase();
-  return inputEdited;
+const playerInput = document.querySelectorAll('.select');
+const playerSelectionDisplay = document.querySelector('.player-selection');
+const computerSelectionDisplay = document.querySelector('.computer-selection');
+const resultsDisplay = document.querySelector('.results')
+
+function displaySelection(playerInput) {
+  playerSelectionDisplay.textContent = '';
+  const img = document.createElement('img');
+  img.setAttribute("src",`${playerInput[0]}`);
+  img.setAttribute("src",`${playerInput[0]}`);
+  playerSelectionDisplay.appendChild(img);
 }
 
+function displayComputerSelection(computerInput) {
+  computerSelectionDisplay.textContent = '';
+  const img = document.createElement('img');
+  if(computerInput === "rock") {
+    img.setAttribute("src","img/fist.png");
+    img.setAttribute("alt", "Rock");
+  }
+  else if(computerInput === "paper") {
+    img.setAttribute("src", "img/open-hand (1).png");
+    img.setAttribute("alt", "Paper")
+  }
+  else {
+    img.setAttribute("src", "img/scissors (1).png")
+    img.setAttribute("alt", "Scissor")
+  }
+  computerSelectionDisplay.appendChild(img);
+}
+
+function getUserPlay(event) {
+  const selectionImg = event.target.getAttribute('src');
+  const selectionAlt = event.target.getAttribute('alt').toLowerCase();
+  return [selectionImg, selectionAlt]
+}
+
+playerInput.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    let userPlay = getUserPlay(event);
+    let computerPlay = getComputerPlay();
+    displaySelection(userPlay);
+    displayComputerSelection(computerPlay);
+    playRound(userPlay, computerPlay);
+  })
+})
+
+
 // Computer inputs a random choice, either “rock”, “paper” or “scissor”
+
 function getComputerPlay() {
   const gameValues = ['rock', 'paper', 'scissor'];
   const randomInput = gameValues[getRandom(0, gameValues.length)];
@@ -17,38 +60,42 @@ function getRandom(min, max) {
 }
 
 // Stores the game scores
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = document.querySelector('.player-score');
+let computerScore = document.querySelector('.machine-score');
+
+playerScore.textContent = 0;
+computerScore.textContent = 0;
+
 
 // Compare the User and Computer inputs and output a result
 function playRound(playerSelection, computerSelection) {
-  if(playerSelection === computerSelection) {
-    console.log("It's a tie");
+  if(playerSelection[1] === computerSelection) {
+    resultsDisplay.textContent = "It's a tie";
     return 'tie';
   }
-  else if(playerSelection === 'rock' && computerSelection === 'scissor') {
-    console.log("You won! Rock beats Scissor");
-    return ++playerScore
+  else if(playerSelection[1] === 'rock' && computerSelection === 'scissor') {
+    resultsDisplay.textContent = "You won! Rock beats Scissor";
+    return ++playerScore.textContent
   }
-  else if(playerSelection === 'rock' && computerSelection === 'paper') {
-    console.log("You lose! Paper beats Rock");
-    return ++computerScore
+  else if(playerSelection[1] === 'rock' && computerSelection === 'paper') {
+    resultsDisplay.textContent = "You lose! Paper beats Rock";
+    return ++computerScore.textContent
   }
-  else if(playerSelection === 'scissor' && computerSelection === 'paper') {
-    console.log("You won! Scissor beats Paper");
-    return ++playerScore
+  else if(playerSelection[1] === 'scissor' && computerSelection === 'paper') {
+    resultsDisplay.textContent = "You won! Scissor beats Paper";
+    return ++playerScore.textContent
   }
-  else if(playerSelection === 'scissor' && computerSelection === 'rock') {
-    console.log("You lose! Rock beats Scissor");
-    return ++computerScore
+  else if(playerSelection[1] === 'scissor' && computerSelection === 'rock') {
+    resultsDisplay.textContent = "You lose! Rock beats Scissor";
+    return ++computerScore.textContent
   }
-  else if(playerSelection === 'paper' && computerSelection === 'rock') {
-    console.log("You won! Paper beats Rock");
-    return ++playerScore
+  else if(playerSelection[1] === 'paper' && computerSelection === 'rock') {
+    resultsDisplay.textContent = "You won! Paper beats Rock";
+    return ++playerScore.textContent
   }
-  else if(playerSelection === 'paper' && computerSelection === 'scissor') {
-    console.log( "You lose! Scissor beats Paper");
-    return ++computerScore
+  else if(playerSelection[1] === 'paper' && computerSelection === 'scissor') {
+    resultsDisplay.textContent =  "You lose! Scissor beats Paper";
+    return ++computerScore.textContent
   }
 }
 
@@ -71,4 +118,9 @@ function playGame() {
     console.log(`Player Score: ${playerScore} | Machine Score: ${computerScore}`);
   }
   defineWinner(playerScore, computerScore);
+}
+
+function playerInfo() {
+  const div = document.createElement('div');
+  
 }
