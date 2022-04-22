168db17a97d9a420ed1c8ac0fe748e4670b6baf8
Kaio Prates <kaio_prates@hotmail.com>
2022-04-22 08:59:02 -0300

    Add restart functionality

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Get player input
 const playerInput = document.querySelectorAll('.select');
 
-function getUserPlay(event) {
+function getPlayerPlay(event) {
   const selectionImg = event.target.getAttribute('src');
   const selectionAlt = event.target.getAttribute('alt').toLowerCase();
   return [selectionImg, selectionAlt]
-}  
+}
 
 // Display player input
 const playerSelectionDisplay = document.querySelector('.player-selection');
 
-function displayUserSelection(playerInput) {
+function displayPlayerSelection(playerInput) {
   playerSelectionDisplay.textContent = '';
   const img = document.createElement('img');
   img.setAttribute("src",`${playerInput[0]}`);
@@ -58,7 +58,7 @@ let computerScore = document.querySelector('.machine-score');
 playerScore.textContent = 0;
 computerScore.textContent = 0;
 
-// Compare the User and Computer inputs and output a result
+// Compare the player and Computer inputs and output a result
 const resultsDisplay = document.querySelector('.results');
 const quickWinAudio = document.querySelector('.quick-win-audio');
 const quickLoseAudio = document.querySelector('.quick-lose-audio');
@@ -67,42 +67,49 @@ const tieAudio = document.querySelector('.tie-audio')
 function playRound(playerSelection, computerSelection) {
   if(playerSelection[1] === computerSelection) {
     resultsDisplay.textContent = "It's a tie";
+    tieAudio.currentTime = 0;
     tieAudio.play();
     return 'tie';
   }  
   else if(playerSelection[1] === 'rock' && computerSelection === 'scissor') {
     resultsDisplay.textContent = "You won! Rock beats Scissor";
+    quickWinAudio.currentTime = 0;
     quickWinAudio.play();
     return ++playerScore.textContent
   }  
   else if(playerSelection[1] === 'rock' && computerSelection === 'paper') {
     resultsDisplay.textContent = "You lose! Paper beats Rock";
+    quickLoseAudio.currentTime = 0;
     quickLoseAudio.play();
     return ++computerScore.textContent
   }  
   else if(playerSelection[1] === 'scissor' && computerSelection === 'paper') {
     resultsDisplay.textContent = "You won! Scissor beats Paper";
+    quickWinAudio.currentTime = 0;
     quickWinAudio.play();
     return ++playerScore.textContent
   }  
   else if(playerSelection[1] === 'scissor' && computerSelection === 'rock') {
     resultsDisplay.textContent = "You lose! Rock beats Scissor";
+    quickLoseAudio.currentTime = 0;
     quickLoseAudio.play();
     return ++computerScore.textContent
   }  
   else if(playerSelection[1] === 'paper' && computerSelection === 'rock') {
     resultsDisplay.textContent = "You won! Paper beats Rock";
+    quickWinAudio.currentTime = 0;
     quickWinAudio.play();
     return ++playerScore.textContent
   }  
   else if(playerSelection[1] === 'paper' && computerSelection === 'scissor') {
     resultsDisplay.textContent =  "You lose! Scissor beats Paper";
+    quickLoseAudio.currentTime = 0;
     quickLoseAudio.play();
     return ++computerScore.textContent
   }  
 }  
 
-// Compare the User and the Computer Scores and return the winner
+// Compare the player and the Computer Scores and return the winner
 const winnerDisplay = document.querySelector('.winner-bg');
 const loserDisplay = document.querySelector('.loser-bg');
 const winnerAudio = document.querySelector('.win-audio');
@@ -118,21 +125,21 @@ function defineWinner(pScore, cScore) {
   }  
 }  
 
-// Get user name and display it
+// Get player name and display it
 const playerName = document.querySelector('.player-name');
-const userName = document.querySelector('#name');
+const nameInput = document.querySelector('#name');
 
-function getUserName(event) {
+function setPlayerName(event) {
   playerName.textContent = event.target.value;
 }
 
-userName.addEventListener('change', getUserName);
+nameInput.addEventListener('change', setPlayerName);
 
-// Get user genre and change icon
+// Get player genre and change icon
 const playerImg = document.querySelector('.player-img');
-const userGenre = document.querySelector('#genre');
+const playerGenre = document.querySelector('#genre');
 
-function setUserImg(select) {
+function setPlayerImg(select) {
   const img = document.createElement('img');
   if (select === 'boy') {
     img.setAttribute("src", "img/boy.png");
@@ -143,33 +150,54 @@ function setUserImg(select) {
   playerImg.appendChild(img);
 }
 
-// Play game
+console.log(playerGenre )
+
+
+// Start and Restart game
 const startBtn = document.querySelector('.start-btn');
 const restartBtn = document.querySelectorAll('.restart-btn');
 const initialScreen = document.querySelector('.info-bg');
+
+function startGame() {
+  const select = playerGenre.options[playerGenre.selectedIndex].value;
+  setPlayerImg(select);
+  initialScreen.style.display = 'none';
+}
+
+function restartGame () {
+  winnerDisplay.style.display = 'none';
+  loserDisplay.style.display = 'none';
+  resultsDisplay.textContent = '';
+  roundNumber.textContent = 1;
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  playerSelectionDisplay.textContent = 'Start Playing';
+  computerSelectionDisplay.textContent = 'Start Playing';
+}
+
+startBtn.addEventListener('click', startGame);
+restartBtn.forEach((item) => {
+  item.addEventListener('click', restartGame);
+})
+
+// Play Game
 const roundNumber = document.querySelector('.round-n');
 
-function playGame() {
-  const select = userGenre.options[userGenre.selectedIndex].value;
-  setUserImg(select);
-  initialScreen.style.display = 'none';
-  playerInput.forEach((item) => {
-    item.addEventListener('click', (event) => {
-      if(roundNumber.textContent <= 5) {
-        let userPlay = getUserPlay(event);
-        displayUserSelection(userPlay);
-        let computerPlay = getComputerPlay();
-        displayComputerSelection(computerPlay);
-        if (playRound(userPlay, computerPlay) === 'tie') {
-          roundNumber.textContent--
+playerInput.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if(roundNumber.textContent <= 5) {
+      let playerPlay = getPlayerPlay(event);
+      displayPlayerSelection(playerPlay);
+      let computerPlay = getComputerPlay();
+      displayComputerSelection(computerPlay);
+      if (playRound(playerPlay, computerPlay) === 'tie') {
+        roundNumber.textContent--
+      }
+      roundNumber.textContent++;
+      console.log(roundNumber.textContent)
+      if (roundNumber.textContent == 6) {
+          defineWinner(playerScore.textContent, computerScore.textContent);
         }
-        roundNumber.textContent++;
-        console.log(roundNumber.textContent)
-        if (roundNumber.textContent == 6) {
-            defineWinner(playerScore.textContent, computerScore.textContent);
-          }
-        }
-    })});
-  }
-
-startBtn.addEventListener('click', playGame);
+      }
+  })});
+  
